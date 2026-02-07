6021b3cb6e21418c30bc38361b3483462c33814c
jump <jump@jump-SpirtBerry-xubuntu>
2026-02-06 23:05:43 -0500

    Revert "completely refacted the project to be an addon that loads the feature set"
    This reverts commit 13d9239f848f949a13c4c340b4f04df6d61bd4e8.

--- 6_bird_feature_set/__init__.py
+++
@@ -1,7 +0,0 @@
-rigify_info = {
-    "name": "6 Bird Studio Rigify Feature Set",
-    "author": "6 Bird Studio",
-    "description":
-        "A series of rig additions as a feature set of the Rigify Extension for Blender.",
-    "link": "https://6bird.studio",
-}
--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 # Rigify-6Bird-Tools
-
-
-
-
-
-
 A series of rig additions as a feature set of the Rigify Extension for Blender.
+
 Current Tools include:
 * Value Slider
 * Slider Pad
